17f3d62123b5bd43bbcb6155f0069f91254b7137
Sergiu Vasilache <sergiu.vasilache@ness.com>
2026-02-01 22:12:35 +0200

    0407 Custom Button Component

--- 04 New Poject/src/App.tsx
+++ 04 New Poject/src/App.tsx
@@ -1,11 +1,22 @@
-import Input from "./components/Input";
+import Button from "./components/Button";
+
 
 
 function App() {
   return (
     <main>
-      <Input label="Your Name" id="name" type="text"/>
-      <Input label="Your Age" id="age" type="number" />
+      <p>
+      <Button el="button" onClick={() => alert('Button clicked!')}>
+        Click Me
+      </Button>
+      </p>
+      <p>
+      <Button el="anchor" href="https://www.google.com">
+        Go to Google
+      </Button>
+      </p>
+      {/* <Input label="Your Name" id="name" type="text"/>
+      <Input label="Your Age" id="age" type="number" /> */}
     </main>
   );
 }
