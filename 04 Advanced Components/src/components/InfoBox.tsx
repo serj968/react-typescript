0f139dd985ad9561af22dd4efaf0d7d3c6b1ac4c
Sergiu Vasilache <sergiu.vasilache@ness.com>
2026-02-01 21:11:16 +0200

    0403 Discriminated Union

--- 04 Advanced Components/src/components/InfoBox.tsx
+++ 04 Advanced Components/src/components/InfoBox.tsx
@@ -1,11 +1,25 @@
 import { type ReactNode } from 'react';
 
-type InfoBoxProps = {
-  mode: 'hint' | 'warning';
+type HintBoxProps = {
+  mode: 'hint';
   children: ReactNode;
 };
 
-export default function InfoBox({ mode, children }: InfoBoxProps) {
+type WarningBoxProps = {
+  mode: 'warning';
+  severity: 'low' | 'medium' | 'high';
+  children: ReactNode;
+};
+
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+export default function InfoBox(props: InfoBoxProps) {
+  
+  //If it is a hint box, severity is not defined
+  const { children, mode } = props;
+
+
   if (mode === 'hint') {
     return (
       <aside className="infobox infobox-hint">
@@ -14,8 +28,11 @@ export default function InfoBox({ mode, children }: InfoBoxProps) {
     );
   }
 
+  //Here, mode is 'warning'
+  const { severity } = props;
+
   return (
-    <aside className="infobox infobox-warning warning--medium">
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
       <p>{children}</p>
     </aside>
