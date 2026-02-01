102803784c50efb29fc7441bdd5d03bc1af241f4
Sergiu Vasilache <sergiu.vasilache@ness.com>
2026-02-01 21:52:20 +0200

    0406 Wrapper with ComponentProps

---
+++ 04 New Poject/src/App.tsx
@@ -0,0 +1,13 @@
+import Input from "./components/Input";
+
+
+function App() {
+  return (
+    <main>
+      <Input label="Your Name" id="name" type="text"/>
+      <Input label="Your Age" id="age" type="number" />
+    </main>
+  );
+}
+
+export default App;
